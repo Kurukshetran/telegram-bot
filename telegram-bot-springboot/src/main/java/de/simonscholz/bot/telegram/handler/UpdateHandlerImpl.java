86a8f67ecd5901e1335f9e30d02a3cabe2545686
Simon Scholz <simon.scholz@vogella.com>
2018-01-05 12:50:28 +0100

    Let the UpdateHandler work with sent location
    Change-Id: I6dff5dd23f8e1a79ccdcbff1f0becd7e89172213
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- telegram-bot-springboot/src/main/java/de/simonscholz/bot/telegram/handler/UpdateHandlerImpl.java
+++ telegram-bot-springboot/src/main/java/de/simonscholz/bot/telegram/handler/UpdateHandlerImpl.java
@@ -7,9 +7,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import de.simonscholz.bot.telegram.api.Location;
 import de.simonscholz.bot.telegram.api.Message;
 import de.simonscholz.bot.telegram.api.Update;
 import de.simonscholz.bot.telegram.bot.TelegramBotClient;
+import de.simonscholz.bot.telegram.location.OSMLocation;
+import de.simonscholz.bot.telegram.location.OpenStreetMapApi;
 import de.simonscholz.bot.telegram.translate.Translation;
 import de.simonscholz.bot.telegram.translate.TranslationApi;
 import de.simonscholz.bot.telegram.weather.DmiApi;
@@ -27,9 +30,12 @@ public class UpdateHandlerImpl implements UpdateHandler {
 
 	@Autowired
 	private TranslationApi translationApi;
-	
+
 	@Autowired
 	private TelegramBotClient telegramBot;
+
+	@Autowired
+	private OpenStreetMapApi locationApi;
 
 	@Override
 	public void handleUpdate(Update update) {
@@ -37,43 +43,53 @@ public class UpdateHandlerImpl implements UpdateHandler {
 
 		int chatId = message.getChat().getId();
 		String text = message.getText();
+		Location location = message.getLocation();
 
-		LOG.debug("Chat id:" + chatId);
-		LOG.debug("Text : " + text);
+		if (text != null) {
+			LOG.debug("Chat id:" + chatId);
+			LOG.debug("Text : " + text);
 
-		int indexOf = text.indexOf(" ");
+			int indexOf = text.indexOf(" ");
 
-		if (indexOf > -1) {
-			String queryString = text.substring(indexOf);
+			if (indexOf > -1) {
+				String queryString = text.substring(indexOf);
 
-			if (text.startsWith("/now")) {
-				Single<List<DmiCity>> dmiCities = dmiApi.getDmiCities(queryString.trim());
-				sendDmiPhoto(chatId, dmiCities, DmiApi.MODE_NOW);
-			} else if (text.startsWith("/week")) {
-				Single<List<DmiCity>> dmiCities = dmiApi.getDmiCities(queryString.trim());
-				sendDmiPhoto(chatId, dmiCities, DmiApi.MODE_WEEK);
-			} else if (text.startsWith("/de")) {
-				Single<Translation> translation = translationApi.getTranslation(queryString, "de", "en");
-				translation.subscribe(t -> {
-					Maybe<Message> sendMessage = telegramBot.sendMessage(chatId, t.getTranslationText());
-					sendMessage.subscribe(m -> {
-						LOG.debug(m.getText());
+				if (text.startsWith("/now")) {
+					Single<List<DmiCity>> dmiCities = dmiApi.getDmiCities(queryString.trim());
+					sendDmiPhoto(chatId, dmiCities, DmiApi.MODE_NOW);
+				} else if (text.startsWith("/week")) {
+					Single<List<DmiCity>> dmiCities = dmiApi.getDmiCities(queryString.trim());
+					sendDmiPhoto(chatId, dmiCities, DmiApi.MODE_WEEK);
+				} else if (text.startsWith("/de")) {
+					Single<Translation> translation = translationApi.getTranslation(queryString, "de", "en");
+					translation.subscribe(t -> {
+						Maybe<Message> sendMessage = telegramBot.sendMessage(chatId, t.getTranslationText());
+						sendMessage.subscribe(m -> {
+							LOG.debug(m.getText());
+						});
 					});
-				});
-			} else if (text.startsWith("/en")) {
-				Single<Translation> translation = translationApi.getTranslation(queryString, "en", "de");
-				translation.subscribe(t -> {
-					Maybe<Message> sendMessage = telegramBot.sendMessage(chatId, t.getTranslationText());
-					sendMessage.subscribe(m -> {
-						LOG.debug(m.getText());
+				} else if (text.startsWith("/en")) {
+					Single<Translation> translation = translationApi.getTranslation(queryString, "en", "de");
+					translation.subscribe(t -> {
+						Maybe<Message> sendMessage = telegramBot.sendMessage(chatId, t.getTranslationText());
+						sendMessage.subscribe(m -> {
+							LOG.debug(m.getText());
+						});
 					});
+				}
+
+			} else {
+				Maybe<Message> sendMessage = telegramBot.sendMessage(chatId,
+						"This is not a proper command. \n Please use /now or /week + city name or for translations /en or /de");
+				sendMessage.subscribe(m -> {
+					LOG.debug(m.getText());
 				});
 			}
-
-		} else {
-			Maybe<Message> sendMessage = telegramBot.sendMessage(chatId, "This is not a proper command. \n Please use /now or /week + city name or for translations /en or /de");
-			sendMessage.subscribe(m -> {
-				LOG.debug(m.getText());
+		} else if (location != null) {
+			Single<OSMLocation> locationData = locationApi.getLocationData(location.getLatitude(), location.getLongitude());
+			locationData.subscribe(l -> {
+				Single<List<DmiCity>> dmiCities = dmiApi.getDmiCities(l.getAddress().getState());
+				sendDmiPhoto(chatId, dmiCities, DmiApi.MODE_NOW);
 			});
 		}
 	}
