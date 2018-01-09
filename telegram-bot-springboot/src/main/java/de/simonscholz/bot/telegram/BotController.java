e15d10b533e70bf509f849d036f83733b937b0de
Simon Scholz <simon.scholz@vogella.com>
2018-01-09 02:12:17 +0100

    Added logger and make use of new Get and Post annotations
    Change-Id: Ie86d024c3ca18e76f7af74fdedcca96a628212e3
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- telegram-bot-springboot/src/main/java/de/simonscholz/bot/telegram/BotController.java
+++ telegram-bot-springboot/src/main/java/de/simonscholz/bot/telegram/BotController.java
@@ -3,10 +3,12 @@ package de.simonscholz.bot.telegram;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,8 @@ import io.reactivex.Maybe;
 
 @RestController
 public class BotController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(BotController.class);
 
 	@Autowired
 	private UpdateHandler handler;
@@ -25,32 +29,32 @@ public class BotController {
 	@Autowired
 	private TelegramBotClient botClient;
 
-	@RequestMapping(value = "/webhook", method = RequestMethod.POST)
+	@PostMapping(value = "/webhook")
 	public void webhook(@RequestBody Update update) {
 		handler.handleUpdate(update);
 	}
 
-	@RequestMapping(value = "/poll", method = RequestMethod.GET)
+	@GetMapping(value = "/poll")
 	public String poll(@RequestParam(name="count", required=false, defaultValue="1") int count) {
 		Maybe<TelegramResponse<Update>> updatesMaybe = botClient.getUpdates();
 		updatesMaybe.subscribe(res -> {
 			List<Update> updates = res.getResult();
 			updates.stream().limit(count).forEach(this::webhook);
 		}, err -> {
-			err.printStackTrace();
+			LOG.error(err.getMessage(), err);
 		});
 		return "Polling updates";
 	}
 	
-	@RequestMapping(value = "/printUpdates", method = RequestMethod.GET)
+	@GetMapping(value = "/printUpdates")
 	public void getUpdates() {
 		Maybe<TelegramResponse<Update>> updatesMaybe = botClient.getUpdates();
 		updatesMaybe.subscribe(res -> {
 			List<Update> updates = res.getResult();
 			String collect = updates.stream().map(update -> update.getMessage().getText()).collect(Collectors.joining(","));
-			System.out.println(collect);
+			LOG.info(collect);
 		}, err -> {
-			err.printStackTrace();
+			LOG.error(err.getMessage(), err);
 		});
 	}
 }
